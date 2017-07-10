efdc2e194f74396b82a5e37f798ba83e875a529d
Гвоздев Михаил Васильевич <mgvozdev@users.vgtrk>
2017-07-10 16:20:45 +0300

    v

--- src/app/shared/channel.ts
+++ src/app/shared/channel.ts
@@ -2,14 +2,14 @@ export class Channel {
   
 
   constructor(
-    public Description: string;
-    public IconUrl: string;
-    public LogoUrl: string;
-    public Id: number;
-    public Guid: string;
-    public Title: string;
-    public Url: string;
+    public Description: string,
+    public IconUrl: string,
+    public LogoUrl: string,
+    public Id: number,
+    public Guid: string,
+    public Title: string,
+    public Url: string,
   ) {
-    
+
   }
 }
