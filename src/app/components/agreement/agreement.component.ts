91c3fe312f4d7540962d31f9a85d9199bec1e3f3
Barmaley86 <vk_31@mail.ru>
2017-12-17 18:49:53 +0300

    finish agreement component

--- src/app/components/agreement/agreement.component.ts
+++ src/app/components/agreement/agreement.component.ts
@@ -22,6 +22,8 @@ export class AgreementComponent extends CommonComponent implements OnInit, OnDes
 
     currentAgreement: Agreement;
     acceptDisabled: boolean = true;
+    loading: boolean = false;
+    error: boolean = false; //флаг от зацикливания показа соглашений при ошибке
 
     constructor(    
         private router: Router,
@@ -45,20 +47,22 @@ export class AgreementComponent extends CommonComponent implements OnInit, OnDes
     }
 
     loadAgreements() {
-        //console.log('loadAgreements');
-        this.userAgreement.getAgreements(this.authService.sessionId)
-        .takeWhile(() => this.alive)  
-        .subscribe( (res: any) => {  
-            const data = this.respondHandler(res);
-            if (data.Data.length > 0) {
-                this.userAgreement.add( data.Data[0] );
-            }
-            else {
-                this.refreshPage();
-            }
-        }, 
-            error => this.errorHandler(error)
-        );
+        if (!this.error) { //флаг от зацикливания показа соглашений при ошибке
+            //console.log('loadAgreements');
+            this.userAgreement.getAgreements(this.authService.sessionId)
+            .takeWhile(() => this.alive)  
+            .subscribe( (res: any) => {  
+                const data = this.respondHandler(res);
+                if (data.Data.length > 0) {
+                    this.userAgreement.add( data.Data[0] );
+                }
+                else {
+                    this.refreshPage();
+                }
+            }, 
+                error => this.errorHandler(error)
+            );
+        }
     }
 
     addFullAgreement(agreement: Agreement) { //получаем полное соглашение
@@ -77,7 +81,7 @@ export class AgreementComponent extends CommonComponent implements OnInit, OnDes
             setTimeout( ()=> {
                 $('#last_agreement').modal({ 
                     closable: false,
-                    onShow: function(){
+                    onVisible: function(){
                         that.modalShowHandler();
                     },
                     onHidden: function(){
@@ -94,12 +98,20 @@ export class AgreementComponent extends CommonComponent implements OnInit, OnDes
         });        
     }
 
-
-
-    onScroll(event: any) {
-        if ( (event.target.scrollTop + event.target.offsetHeight) == event.target.scrollHeight) {
-            this.acceptDisabled = false;
-        }
+    acceptAgreement() {
+        this.userAgreement.acceptAgreement(this.currentAgreement.AcceptToken)
+        .takeWhile(() => this.alive)  
+        .subscribe( (status: number) => {             
+                if (status == 204) {
+                    $('#last_agreement').modal('hide');
+                }
+            },
+            error => {                
+                this.error = true; //флаг от зацикливания показа соглашений при ошибке
+                this.errorHandler(error);
+                $('#last_agreement').modal('hide');
+            }
+        );
     }
 
     modalShowHandler() { //проверка на наличие скролла, вкучение кнопки при его отсутствии
@@ -110,21 +122,22 @@ export class AgreementComponent extends CommonComponent implements OnInit, OnDes
     }
 
     modalHideHandler() {
-        this.loadAgreements(); //проверка на оставшиеся соглашения
+        this.loading = false;
         this.userAgreement.clear();
-        this.acceptDisabled = true;
+        this.loadAgreements(); //проверка на оставшиеся соглашения        
+        //this.acceptDisabled = true;
+    }
+
+    onScroll(event: any) {
+        if ( !this.loading && (event.target.scrollTop + event.target.offsetHeight) == event.target.scrollHeight) {
+            this.acceptDisabled = false;
+        }
     }
 
     accept() {
-        this.userAgreement.acceptAgreement(this.currentAgreement.AcceptToken)
-        .takeWhile(() => this.alive)  
-        .subscribe( (status: number) => {  
-            if (status == 204) {
-                $('#last_agreement').modal('hide');
-            }
-        }, 
-            error => this.errorHandler(error)
-        );
+        this.loading = true;
+        this.acceptDisabled = true;
+        this.acceptAgreement();        
     }
 
     refreshPage() {
